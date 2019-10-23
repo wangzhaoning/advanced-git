80220dc5bd67e31c2f7992888c19db05800dd43e
wangzhaoning <wangzhaoning@inspur.com>
2019-10-23 16:50:29 +0800

    fix a bug

--- Product.java
+++ Product.java
@@ -1,7 +1,5 @@
 public class Product {
     public static void main(String[] args) {
         System.out.println("I'm version 9.1!");
-        System.out.println("I have a bug!");
-        System.out.println("A bad feature!");
     }
 } 
